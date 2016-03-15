a88d8bd574413d50ee3ba78e7676368af6e4bc4e
Sergey Kanukov <s.kanukov@gmail.com>
2016-03-15 09:57:59 +0300

    Add latest stable Bootstrap

---
+++ solidify/templates/solidus/copy/app/assets/javascripts/bootstrap_custom.js
@@ -0,0 +1,14 @@
+// Exclude everything by default. Enable components you use.
+
+// require bootstrap/affix
+// require bootstrap/alert
+// require bootstrap/button
+// require bootstrap/carousel
+// require bootstrap/collapse
+// require bootstrap/dropdown
+// require bootstrap/modal
+// require bootstrap/scrollspy
+// require bootstrap/tab
+// require bootstrap/transition
+// require bootstrap/tooltip
+// require bootstrap/popover
